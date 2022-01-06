6fdd7998305ba1ff029381e750f92105d6f0dd73
vhoc <vhocar@gmail.com>
2022-01-06 12:00:31 -0700

    added readme

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const hostname = 'dnatest.victorolvera.net';
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/dnatest', { useNewUrlParser: true });
@@ -9,4 +10,4 @@ app.use(express.json());
 const dnaTestRouter = require('./routes/DnaRoutes')
 app.use('/', dnaTestRouter)
 
-app.listen(3001, () => console.log("Service started."));
+app.listen(80, hostname, () => console.log("Service started."));
