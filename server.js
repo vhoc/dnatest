3a4ba7a6b1ccf17633d82c08f3cf69349ed5ca27
Victor Olvera <vhocar@gmail.com>
2022-01-06 19:21:45 -0700

    cors

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ mongoose.connect('mongodb://localhost/dnatest', { useNewUrlParser: true });
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 const dnaTestRouter = require('./routes/DnaRoutes')
 app.use('/', dnaTestRouter)
 
